713ab0f6732aecd7a01e2a79e724c2ae22bccf20
arashsofts <googmake3@gmail.com>
2017-12-25 16:23:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe 
 
 setup(
-    console=['sheeba.py'])
+    console=['sheeba.py'],
     options = {
         'py2exe' = {
             'packages': ['getpass']

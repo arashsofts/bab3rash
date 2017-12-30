f3847338750a6b10ee3f8045241041b44aae51da
arashsofts <googmake3@gmail.com>
2017-12-30 07:01:37 -0500

    Update and rename sked.txt to sked.py

--- sked.py
+++ sked.py
@@ -1,5 +1,5 @@
-Abdullah;Monday:4-Close;Tuesday:---;Wednesday:4-Close;Thursday:4-Close;Friday:---;Saturday:4-Close;Sunday:4-Close
+'''Abdullah;Monday:4-Close;Tuesday:---;Wednesday:4-Close;Thursday:4-Close;Friday:---;Saturday:4-Close;Sunday:4-Close
 Abdulrahman;Monday:4-Close;Tuesday:---;Wednesday:---;Thursday:---;Friday:9-4;Saturday:4-Close;Sunday:---
 Ahmed;Monday:4-Close;Tuesday:---;Wednesday:---;Thursday:---;Friday:4-Close;Saturday:---;Sunday:4-Close
 Fakri;Monday:---;Tuesday:---;Wednesday:---;Thursday:---;Friday:---;Saturday:9-4;Sunday:---
-Osamah;Monday:9-4;Tuesday:9-4;Wednesday:9-4;Thursday:9-4;Friday:4-Close;Saturday:---;Sunday:---
+Osamah;Monday:9-4;Tuesday:9-4;Wednesday:9-4;Thursday:9-4;Friday:4-Close;Saturday:---;Sunday:---'''
